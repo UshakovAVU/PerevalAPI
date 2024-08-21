6e0a3636e93ed8daf20b3b8607f52d375ddb2ac2
Oleg <benetal@yandex.ru>
2024-08-21 15:02:40 +0700

    Add swagger

--- pereval/pereval/settings.py
+++ pereval/pereval/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'django_filters',
     'mountpass.apps.MountpassConfig',
     'rest_framework',
--- pereval/pereval/urls.py
+++ pereval/pereval/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from mountpass import views
+from .yasg import urlpatterns as doc_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,4 @@ urlpatterns = [
     path('api/submitdata/<int:pk>/', views.PerevalViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'}),
                                                                     name='pereval-detail'),
 ]
+urlpatterns += doc_url
